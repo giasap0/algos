1b70a5bbf2e6e6a53210696eb753566f84cf28ee
Giampaolo Saporito <saporito.gia@gmail.com>
2018-07-21 20:26:54 +0200

    update to angular 6

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,41 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-/** bootstrap modules */
-import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
-import { CollapseModule } from 'ngx-bootstrap/collapse';
-
 import { AppComponent } from './app.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './/app-routing.module';
-
-/** components */
-import { LoginComponent } from './components/login/login.component';
-import { SignalCreationComponent } from './components/signal-creation/signal-creation.component';
-
-
 /** services */
 import { TipiProdottoService } from './service/tipi-prodotto.service';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { FooterComponent } from './components/footer/footer.component';
-
-
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-	SignalCreationComponent,
-	NavbarComponent,
-	SidebarComponent,
-	FooterComponent
+    AppComponent
   ],
   imports: [
-	BrowserModule,
-	FormsModule,
-	BsDropdownModule.forRoot(),
-	CollapseModule.forRoot(),
-	AppRoutingModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [ TipiProdottoService ],
   bootstrap: [AppComponent]
